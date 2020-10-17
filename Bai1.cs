f030e26800c8cc375b8a30313ed1362679a24578
LeTruong2701 <72636049+LeTruong2701@users.noreply.github.com>
2020-10-17 15:04:32 +0700

    day code bai 1

---
+++ Bai1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaiTH2.LTHDT
+{
+    class Stack
+    {
+        private int top;
+        private int[] s;
+        public bool empty()
+        {
+            return (top == -1);
+        }
+        public bool full()
+        {
+            return (top >= s.Length);
+        }
+        public Stack()
+        {
+            s = new int[20];
+            top = -1;
+        }
+        public void push(int x)
+        {
+            if (!full())
+            {
+                top = top + 1;
+                s[top] = x;
+            }
+            else
+                Console.Write("stack tran");
+        }
+        public int pop()
+        {
+            if (empty())
+            {
+                Console.Write("stack can");
+                return 0;
+            }
+            else
+                return s[top--];
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaiTH2.LTHDT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ////bai1
+            //int n;
+            //Console.Write("Nhap vao so can doi:");
+            //n = int.Parse(Console.ReadLine());
+            //Stack T = new Stack();
+            //string st = "0123456789ABCDEF";
+            //while (n != 0)
+            //{
+            //    T.push((int)st[n % 16]);
+            //    n = n / 16;
+            //}
+            //Console.Write("Ket qua doi sang he 16 :");
+            //while (!T.empty())
+            //{
+            //    Console.Write("{0}", (char)T.pop());
+            //}
+            Console.ReadLine();
+        }
+    }
+}
